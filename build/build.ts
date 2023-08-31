c4137a3b523c7d08f4c2c751ec1a1058c3f3c5c7
tjjfvi <t6@t6.fyi>
2023-08-31 16:15:22 -0400

    add watch mode

--- build/build.ts
+++ build/build.ts
@@ -2,63 +2,76 @@ import * as colors from "https://deno.land/std@0.200.0/fmt/colors.ts"
 import { encodeHex } from "https://deno.land/x/hexes@v0.1.0/encode.ts"
 import { codegen } from "./codegen.ts"
 
-interface BuildProps {
-  watPath: string
+export interface BuildOpts {
   wabtArgs: string[]
   dynamic: boolean
   check: boolean
 }
 
-export async function build({ watPath, wabtArgs, dynamic, check }: BuildProps): Promise<boolean> {
-  const wasmCmd = await new Deno.Command("wat2wasm", {
-    args: [watPath, ...wabtArgs, "--output=-"],
-    stdout: "piped",
-    stderr: "piped",
-    stdin: "null",
-    env: {
-      FORCE_COLOR: colors.getColorEnabled() ? "1" : "0",
-    },
-  }).output()
+export type BuildStatus = Xor<
+  | { type: "dirty"; ok: false }
+  | { type: "clean" | "built"; bytes: number; ok: true }
+  | { type: "going"; controller: AbortController }
+  | { type: "error"; error: string; ok: false }
+>
 
-  if (!wasmCmd.success) {
-    console.log(tag("error", colors.bgRed), watPath)
-    console.log(new TextDecoder().decode(wasmCmd.stderr).replace(/^/gm, "  "))
-    return false
-  }
+type Expand<T> = T extends T ? { [K in keyof T]: T[K] } : never
+type Xor<T, K extends keyof any = T extends T ? keyof T : never> = T extends T
+  ? Expand<T & Partial<Record<Exclude<K, keyof T>, never>>>
+  : never
 
-  const wasm = wasmCmd.stdout
-  const code = codegen(wasm, dynamic)
+export async function build(
+  watPath: string,
+  { wabtArgs, dynamic, check }: BuildOpts,
+  signal?: AbortSignal,
+): Promise<BuildStatus> {
+  try {
+    const wasmCmd = await new Deno.Command("wat2wasm", {
+      args: [watPath, ...wabtArgs, "--output=-"],
+      stdout: "piped",
+      stderr: "piped",
+      stdin: "null",
+      env: {
+        FORCE_COLOR: colors.getColorEnabled() ? "1" : "0",
+      },
+      signal,
+    }).output()
 
-  const hash = encodeHex(
-    new Uint8Array(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(code))),
-  )
-  const generated = `@generated ${hash}`
-
-  const outputPath = watPath + ".ts"
-  const existing = await Deno.readTextFile(outputPath).catch(() => "")
-  const clean = existing.includes(generated)
-
-  const byteCount = colors.gray(`(${wasm.length} bytes)`)
-
-  if (check) {
-    if (clean) {
-      console.log(tag("clean", colors.bgBrightGreen), outputPath, byteCount)
-    } else {
-      console.log(tag("dirty", colors.bgBrightRed), outputPath)
+    if (!wasmCmd.success) {
+      const error = new TextDecoder().decode(wasmCmd.stderr)
+      return { type: "error", error, ok: false }
     }
-    return clean
-  } else {
-    if (clean) {
-      console.log(tag("clean", colors.bgBrightBlack), outputPath, byteCount)
-    } else {
-      const file = `// ${generated}\n\n${code}`
-      await Deno.writeTextFile(outputPath, file)
-      console.log(tag("built", colors.bgBlue), outputPath, byteCount)
-    }
-    return true
-  }
-}
 
-function tag(label: string, bgColor: (text: string) => string) {
-  return colors.bold(bgColor(` ${label.toUpperCase()} `))
+    const wasm = wasmCmd.stdout
+    const code = codegen(wasm, dynamic)
+
+    const hash = encodeHex(
+      new Uint8Array(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(code))),
+    )
+    const generated = `@generated ${hash}`
+
+    const outputPath = watPath + ".ts"
+    const existing = await Deno.readTextFile(outputPath).catch(() => "")
+    const clean = existing.includes(generated)
+
+    const bytes = wasm.length
+
+    if (check) {
+      if (clean) {
+        return { type: "clean", bytes, ok: true }
+      } else {
+        return { type: "dirty", ok: false }
+      }
+    } else {
+      if (clean) {
+        return { type: "clean", bytes, ok: true }
+      } else {
+        const file = `// ${generated}\n\n${code}`
+        await Deno.writeTextFile(outputPath, file)
+        return { type: "built", bytes, ok: true }
+      }
+    }
+  } catch (e) {
+    return { type: "error", error: Deno.inspect(e), ok: false }
+  }
 }
--- main.ts
+++ main.ts
@@ -1,33 +1,141 @@
 import { parse } from "https://deno.land/std@0.200.0/flags/mod.ts"
-import { expandGlob } from "https://deno.land/std@0.200.0/fs/expand_glob.ts"
+import * as colors from "https://deno.land/std@0.200.0/fmt/colors.ts"
+import { walk } from "https://deno.land/std@0.200.0/fs/walk.ts"
+import { globToRegExp } from "https://deno.land/std@0.200.0/path/glob.ts"
 import { relative } from "https://deno.land/std@0.200.0/path/relative.ts"
-import { build } from "./build/build.ts"
+import { build, BuildOpts, BuildStatus } from "./build/build.ts"
 
 const args = parse(Deno.args, {
-  boolean: ["dynamic", "check"],
-  collect: ["wabt-arg"],
-  string: ["wabt-arg"],
+  boolean: ["dynamic", "check", "watch"],
+  collect: ["wabt-arg", "exclude"],
+  string: ["wabt-arg", "exclude"],
 })
 
-const files: string[] = []
+const opts: BuildOpts = {
+  wabtArgs: args["wabt-arg"],
+  dynamic: args.dynamic,
+  check: args.check,
+}
 
-await Promise.all(args._.map(async (glob) => {
-  for await (const entry of expandGlob(glob.toString())) {
-    files.push(relative(Deno.cwd(), entry.path))
+const alwaysExclude = [".git"]
+
+const include = args._.map((glob) => globToRegExp(glob as string))
+const exclude = [...args.exclude, ...alwaysExclude].map((glob) => globToRegExp(glob as string))
+
+if (!args.watch) {
+  const paths: string[] = await scan()
+
+  const statuses = new Map(
+    await Promise.all(
+      paths.map(async (path) => [path, await build(path, opts)] as const),
+    ),
+  )
+
+  printStatuses(statuses)
+
+  if (![...statuses.values()].every((status) => status.ok)) {
+    Deno.exit(1)
   }
-}))
+} else {
+  watch()
+}
 
-files.sort()
+async function watch() {
+  let statuses = new Map<string, BuildStatus>(
+    (await scan()).map((path) => [path, queueBuild(path)] as const),
+  )
 
-const results = await Promise.all(files.map((file) =>
-  build({
-    watPath: file,
-    wabtArgs: args["wabt-arg"],
-    dynamic: args.dynamic,
-    check: args.check,
-  })
-))
+  print()
 
-if (results.some((ok) => !ok)) {
-  Deno.exit(1)
+  for await (const event of Deno.watchFs(".")) {
+    let dirty = false
+    if (event.flag === "rescan") {
+      const paths = await scan()
+      statuses.forEach((status) => status.controller?.abort())
+      statuses = new Map(paths.map((path) => [path, statuses.get(path) ?? queueBuild(path)]))
+      dirty = true
+    } else {
+      for (let path of event.paths) {
+        path = relative(Deno.cwd(), path)
+        if (!match(path)) continue
+
+        if (event.kind === "create" || event.kind === "modify" || event.kind === "remove") {
+          try {
+            Deno.statSync(path)
+            statuses.get(path)?.controller?.abort()
+            statuses.set(path, queueBuild(path))
+            dirty = true
+          } catch {}
+        }
+      }
+    }
+
+    if (dirty) {
+      for (const [path, status] of statuses) {
+        if (status.type === "built") {
+          statuses.set(path, { ...status, type: "clean" })
+        }
+      }
+      print()
+    }
+  }
+
+  function queueBuild(path: string): BuildStatus {
+    const controller = new AbortController()
+    build(path, opts, controller.signal).then((status) => {
+      if (!controller.signal.aborted) {
+        statuses.set(path, status)
+        print()
+      }
+    })
+    return { type: "going", controller }
+  }
+
+  function print() {
+    Deno.stdout.writeSync(new Uint8Array([0x1b, 0x63]))
+    printStatuses(statuses)
+    console.log(colors.gray("\nWatching... "))
+  }
+}
+
+function match(path: string) {
+  return include.some((x) => x.test(path)) && !exclude.some((x) => x.test(path))
+}
+
+async function scan(): Promise<string[]> {
+  const files = []
+
+  for await (
+    const entry of walk(".", {
+      match: include,
+      skip: exclude,
+      includeDirs: false,
+      includeSymlinks: false,
+    })
+  ) {
+    files.push(entry.path)
+  }
+
+  return files.sort()
+}
+
+function printStatuses(statuses: Map<string, BuildStatus>) {
+  for (const [path, status] of statuses) {
+    const color = status.type === "built"
+      ? colors.bgGreen
+      : status.type === "clean"
+      ? args.check ? colors.bgGreen : colors.bgBrightBlack
+      : status.type === "dirty"
+      ? colors.bgBrightRed
+      : status.type === "error"
+      ? colors.bgRed
+      : colors.bgBlue
+    const tag = colors.bold(color(` ${status.type.toUpperCase()} `))
+    const extra = status.type === "built" || status.type === "clean"
+      ? [colors.gray(`(${status.bytes} bytes)`)]
+      : status.type === "error"
+      ? ["\n" + status.error.trimEnd().replace(/^/gm, "  ")]
+      : []
+    console.log(tag, path, ...extra)
+  }
 }
